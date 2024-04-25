1f3bed4a591fc0eb0b3465bcc27572c215653275
GauravWin <gauravrsawant1313@gmail.com>
2024-04-25 19:35:23 +0530

    port

--- app.py
+++ app.py
@@ -43,4 +43,5 @@ def predict():
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)  # Check these settings
+
